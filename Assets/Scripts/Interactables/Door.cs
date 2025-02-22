e1935ac6f76038db4dc11404d8e07b52167fdb65
charlotte <cdoo@sfu.ca>
2025-02-21 20:26:45 -0800

    Doors
    Unlock based on puzzle/combat completed

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Door : Interactable
 {
+    public GameManager gameManager; //message saying its locked
+    public int doorNumber;
+
     public string sceneToLoad;
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = FindObjectOfType<GameManager>();
         
     }
 
@@ -20,8 +24,27 @@ public class Door : Interactable
 
     public override void Interact()
     {
+        if(doorNumber == 1){ //open hallway door
+            if(gameManager.hasCompletedPuzzleTut && gameManager.hasCompletedCombatTut){
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }else if(doorNumber == 2){ //open second door
+            if(gameManager.hasCompletedPuzzle1 && gameManager.hasCompletedCombat1){
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }else if(doorNumber == 3){ //open third door
+            if(gameManager.hasCompletedPuzzle2 && gameManager.hasCompletedCombat2){
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+
+        if(doorNumber == 0){ //open hallway door
+            SceneManager.LoadScene(sceneToLoad);
+        }
+
+
         Debug.Log("Interacted with " + gameObject.name);
-        SceneManager.LoadScene(sceneToLoad);
+        
 
     }
 }

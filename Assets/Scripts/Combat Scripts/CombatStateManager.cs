68b0cdf0792353f6a54d05ac294fb0aaff2576c6
charlotte <cdoo@sfu.ca>
2025-02-22 16:24:08 -0800

    Combat Notes

--- Assets/Scripts/Combat Scripts/CombatStateManager.cs
+++ Assets/Scripts/Combat Scripts/CombatStateManager.cs
@@ -40,14 +40,14 @@ public class CombatStateManager : MonoBehaviour
     void Start()
     {
 
-        /*
+        
         gameManager = FindObjectOfType<GameManager>();
 
         if (gameManager != null && gameManager.audioManager != null)
         {
             gameManager.audioManager.StopBackgroundMusic();
         }
-        */
+        
 
         //set up the proper song
         selectSong();
@@ -159,7 +159,7 @@ public class CombatStateManager : MonoBehaviour
     //select the song based on gamamanager id
     void selectSong()
     {
-        /**
+        
         //load proper song
         if (gameManager.currentSong == 001)
         {
@@ -183,8 +183,7 @@ public class CombatStateManager : MonoBehaviour
         }
 
         currentSong.songID = gameManager.currentSong;
-        */
-        currentSong = new ThiefSong();
+        
     }
 
     void CheckModeSwitch(double currentTime)
